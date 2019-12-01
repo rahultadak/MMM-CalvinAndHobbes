fffcc2a38fd9efd6ff649ac52edfa8baaa2dd2e8
rahultadak <rahultadak91@gmail.com>
2019-12-01 11:51:44 -0800

    Getting comit URL all the way to send response

--- node_helper.js
+++ node_helper.js
@@ -1,14 +1,16 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const host = 'gocomics.com'
+const host = 'gocomics.com';
 const comicPath = '/calvinandhobbes/';
+const base = 'https://www.gocomics.com/calvinandhobbes/';
+var today_url = '';
 
 var NodeHelper = require("node_helper");
 module.exports = NodeHelper.create({
 
     start: function () {
         console.log("Starting node helper: " + this.name);
-        this.today_url = '';
+
     },
 
     socketNotificationReceived: function(notification, payload) {
@@ -19,43 +21,64 @@ module.exports = NodeHelper.create({
         }
     },
 
-    fetchComicDom: function () {
-        this.fetchValidComicLinkForToday();
-        axios.get(this.today_url)
-            .then(response => {
-                // console.log(response.data);
-                this.getComicLink(response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-    },
-
     fetchValidComicLinkForToday: function () {
-        var today = new Date();
-        var year = today.getUTCFullYear();
-        var month = today.getUTCMonth() + 1;
-        month = (month < 10 ? '0' : '') + month;
-        var date = (today.getUTCDate() < 10 ? '0' : '') + today.getUTCDate();
-        this.today_url = 'http://gocomics.com/calvinandhobbes/' + year + '/' + month + '/' + date;
-    },
+        return new Promise(function (resolve, reject) {
+            var today = new Date();
+            var year = today.getUTCFullYear();
+            var month = today.getUTCMonth() + 1;
+            month = (month < 10 ? '0' : '') + month;
+            var date = (today.getUTCDate() < 10 ? '0' : '') + today.getUTCDate();
+            today_url = base + year + '/' + month + '/' + date;
 
-    getComicLink: function (html) {
-        console.log("Trying to get comic link")
-        comicUrl = ''
-        const $ = cheerio.load(html);
-        $('div.comic.container').each(function () {
-            comicUrl = $(this).attr('data-image');
-            console.log("Found data  url: " + $(this).attr('data-url'));
-            console.log("Found comic url: " + comicUrl);
+            // Reject if some sort of error
+            console.log(today_url);
+            resolve(today_url);
         });
     },
 
-    sendComic: function () {
-        this.fetchComicDom();
-        comic = {text: this.comicUrl};
+    getComicLink: function (html) {
+        return new Promise( function (resolve, reject) {
+            console.log("Trying to get comic link");
+            const $ = cheerio.load(html);
+            try {
+                // console.log($('div.comic.container'));
+                $('div.comic.container').each(function () {
+                    console.log("Inside each loop" + this);
+                    var data_url = $(this).attr('data-url');
+                    if (data_url === today_url) {
+                        var comicUrl = $(this).attr('data-image');
+                        console.log("Found data  url: " + $(this).attr('data-url'));
+                        console.log("Found comic url: " + comicUrl);
+
+                        resolve(comicUrl);
+                    }
+                    else {
+                        throw Error("Could not find the right Element");
+                    }
+                });
+            }
+            catch (e) {
+                reject(e);
+            }
+        });
+    },
+
+    sendComicNotification: function (in_text) {
+        comic = {text: in_text};
         console.log("Sending new comic");
         this.sendSocketNotification('COMIC', comic);
     },
 
+    sendComic: async function () {
+        try {
+            var url = await this.fetchValidComicLinkForToday();
+            var html = await axios.get(url);
+            var comicUrl = await this.getComicLink(html.data);
+            console.log(comicUrl);
+            this.sendComicNotification(comicUrl);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
 });
